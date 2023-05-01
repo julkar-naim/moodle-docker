f30c6cf14b9b8fb0fc0a40e94f51c7e4f629b2ad
NAIMJX <julkar.naim@abbvie.com>
2023-05-01 14:04:11 +0600

    dockerizing moodle

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM php:7.4-apache
+
+COPY 000-default.conf /etc/apache2/sites-available/000-default.conf
+
+RUN mkdir /var/www/moodle
+RUN chown -R root /var/www/moodle
+RUN chmod -R 0755 /var/www/moodle
+RUN mkdir /var/moodledata
+RUN chmod -R 0777 /var/moodledata
+
+RUN curl -sSL https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions -o - | sh -s \
+      mysqli gd zip intl
+
+CMD ["apache2-foreground"]
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = $_ENV['MYSQL_HOST'];
+$CFG->dbname    = $_ENV['MYSQL_DB'];
+$CFG->dbuser    = $_ENV['MYSQL_USER'];
+$CFG->dbpass    = $_ENV['MYSQL_PASSWORD'];
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'http://localhost:8000';
+$CFG->dataroot  = '/var/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
